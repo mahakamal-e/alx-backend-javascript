01aeeb99bed7a91a6425299a47502cc408f562d6
mahakamal-e <mahakamal09@gmail.com>
2024-06-26 05:09:21 +0000

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,20 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise])
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises)
     .then((results) => {
-      return results.map((result) => ({
+      const modifiedResults = results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message
+        value: result.value ? result.value : String(result.reason),
       }));
-    })
-    .catch((error) => {
-      console.log('Error in handling profile signup:', error);
-      return []; // Return an empty array in case of an error
+
+      return modifiedResults;
     });
 }
-
 export default handleProfileSignup;
