6868c2be8963f641ed308b0e0cd864f52c137ff8
mahakamal-e <mahakamal09@gmail.com>
2024-06-26 04:06:47 +0000

    add

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,15 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Mock API response");
-        }, 1000);
-    });
+  return new Promise((resolve, reject) => {
+    const success = true; // Change this to simulate success or failure
+    setTimeout(() => {
+      if (success) {
+        resolve('Mock API response');
+      } else {
+        reject('Error: API request failed');
+      }
+    }, 1000);
+  });
 }
 
 export default getResponseFromAPI;
+
