5c1db4d7904ffe61a69313b3ce3262d0065c5437
mahakamal-e <mahakamal09@gmail.com>
2024-06-26 05:21:31 +0000

    add task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+
+    return { user, photo };
+  } catch (err) {
+    return { photo: null, user: null };
+  }
+}
