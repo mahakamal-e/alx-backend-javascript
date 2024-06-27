2ba9e8f0ea39fd590d47928f5bae7b017f556894
mahakamal-e <mahakamal09@gmail.com>
2024-06-27 23:05:17 +0000

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
