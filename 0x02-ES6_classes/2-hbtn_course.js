5fd5243d4cfb51152b524e0ef16e7d1f258ec112
mahakamal-e <mahakamal09@gmail.com>
2024-06-27 20:46:57 +0000

    edit

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,55 +1,47 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, "Name");
-    this._length = this.validateNumber(length, "Length");
-    this._students = this.validateArray(students, "Students");
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter and setter for name
   get name() {
     return this._name;
   }
+
   set name(newName) {
-    this._name = this.validateString(newName, "Name");
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = newName;
   }
 
   get length() {
     return this._length;
   }
+
   set length(newLength) {
-    this._length = this.validateNumber(newLength, "Length");
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    this._length = newLength;
   }
 
   get students() {
     return this._students;
   }
+
   set students(newStudents) {
-    this._students = this.validateArray(newStudents, "Students");
-  }
-
-  validateString(value, propName) {
-    if (typeof value !== "string") {
-      throw new TypeError(`${propName} must be a string`);
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
     }
-    return value;
-  }
 
-  validateNumber(value, propName) {
-    if (typeof value !== "number" || isNaN(value)) {
-      throw new TypeError(`${propName} must be a number`);
+    if (!newStudents.every((item) => typeof item === 'string')) {
+      throw new TypeError('All elements in students must be strings');
     }
-    return value;
-  }
 
-  validateArray(value, propName) {
-    if (!Array.isArray(value)) {
-      throw new TypeError(`${propName} must be an array`);
-    }
-    value.forEach((item, index) => {
-      if (typeof item !== "string") {
-        throw new TypeError(`Element at index ${index} of ${propName} must be a string`);
-      }
-    });
-    return value;
+    this._students = newStudents;
   }
 }
