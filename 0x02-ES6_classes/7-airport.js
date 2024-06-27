9a43369eed45083adf2b2a69336a4a5de1dc5cf9
mahakamal-e <mahakamal09@gmail.com>
2024-06-27 23:23:07 +0000

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
