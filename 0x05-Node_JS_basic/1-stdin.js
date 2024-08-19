3d57d1e195e846a5adff3caeb5cec726b1f52217
mahakamal-e <mahakamal09@gmail.com>
2024-08-19 21:18:50 +0000

    edit

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log("Welcome to Holberton School, what is your name?");
+rl.question('', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+  console.log("This important software is now closing");
+});
