821de5bd3ede7c6bbfe1ac1ed341117606907dc4
mahakamal-e <mahakamal09@gmail.com>
2024-08-20 02:11:22 +0000

    fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,86 +1,88 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
-// Reuse the logic from `3-read_file_async.js`
-function countStudents(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+const portNumber = 1245;
+const hostname = 'localhost';
+const dbName = process.argv[2];
 
-      const lines = data.trim().split('\n');
-      if (lines.length <= 1) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+const parseStudentsByField = (dataList) => {
+  const labelsList = dataList[0].split(',');
+  const fieldIdx = labelsList.indexOf('field');
+  const firstNameIdx = labelsList.indexOf('firstname');
 
-      const [, ...rows] = lines;
-      const fieldCounts = {};
-      const fieldNames = {};
+  const studentsByField = {};
+  let studentsCount = 0;
 
-      rows.forEach((row) => {
-        const [firstname, , , field] = row.split(',');
+  for (let i = 1; i < dataList.length; i += 1) {
+    // eslint-disable-next-line no-continue
+    if (dataList[i] === '') continue;
+    studentsCount += 1;
+    const studentList = dataList[i].split(',');
+    const fieldName = studentList[fieldIdx];
+    if (!studentsByField[fieldName]) studentsByField[fieldName] = [];
+    studentsByField[fieldName].push(studentList[firstNameIdx]);
+  }
 
-        // Skip empty lines and rows with missing data
-        if (!firstname || !field) return;
+  return { studentsByField, studentsCount };
+};
 
-        if (!fieldCounts[field]) {
-          fieldCounts[field] = 0;
-          fieldNames[field] = [];
-        }
+const printStudentsWithField = (studentsByField) => {
+  let studentsStr = '';
+  const keys = Object.keys(studentsByField);
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    const firstNamesList = studentsByField[key];
+    const studentsNumber = firstNamesList.length;
+    const firstNamesStr = firstNamesList.join(', ');
+    studentsStr += `Number of students in ${key}: ${studentsNumber}. List: ${firstNamesStr}`;
+    if (i < keys.length - 1) studentsStr += '\n';
+  }
 
-        fieldCounts[field] += 1;
-        fieldNames[field].push(firstname);
-      });
+  return studentsStr;
+};
 
-      const totalStudents = Object.values(fieldCounts).reduce((a, b) => a + b, 0);
-      let output = `Number of students: ${totalStudents}\n`;
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-      for (const [field, count] of Object.entries(fieldCounts)) {
-        const namesList = fieldNames[field].join(', ');
-        output += `Number of students in ${field}: ${count}. List: ${namesList}\n`;
-      }
+    const dataList = data.split('\n');
+    const studentsData = parseStudentsByField(dataList);
+    const { studentsByField } = studentsData;
+    const { studentsCount } = studentsData;
 
-      resolve(output.trim());
-    });
+    let studentStr = `Number of students: ${studentsCount}\n`;
+    studentStr += printStudentsWithField(studentsByField);
+    resolve(studentStr);
   });
-}
+});
 
-// Function to send HTTP response
 const sendResponse = (res, data) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(data);
 };
 
-// Create the HTTP server
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     const responseText = 'Hello Holberton School!';
     sendResponse(res, responseText);
   } else if (req.url === '/students') {
-    const filePath = path.join(__dirname, 'database.csv'); // Path to the database file
-    countStudents(filePath)
-      .then((output) => {
-        const responseText = `This is the list of our students\n${output}`;
+    countStudents(dbName)
+      .then((data) => {
+        const responseText = `This is the list of our students\n${data}`;
         sendResponse(res, responseText);
-      })
-      .catch((error) => {
+      }).catch((error) => {
         const responseText = `This is the list of our students\n${error.message}`;
         sendResponse(res, responseText);
       });
-  } else {
-    sendResponse(res, 'Not Found');
   }
 });
 
-// Listen on port 1245
-app.listen(1245, 'localhost', () => {
-  console.log('Server running at http://localhost:1245/');
+app.listen(portNumber, hostname, () => {
+  console.log(`Server running at http://${hostname}:${portNumber}/`);
 });
 
-// Export the server
 module.exports = app;
