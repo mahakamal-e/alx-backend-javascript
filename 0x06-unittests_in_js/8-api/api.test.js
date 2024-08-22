a194651bd74063b46f359edc9e8d5026a1ea6203
mahakamal-e <mahakamal09@gmail.com>
2024-08-22 17:21:54 +0000

    edit

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,13 @@
 const request = require('request');
-const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
-  it('should return status code 200', (done) => {
-    request('http://localhost:7865', (error, response, body) => {
+const chai = require('chai');
+const expect = chai.expect;
+
+
+describe('getRequest', function () {
+  it('should get index page', function (done) {
+    request.get('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result', (done) => {
-    request('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
